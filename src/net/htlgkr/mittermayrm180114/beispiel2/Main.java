d6ac2dbcda363f07b9a7036e0335ed5c0a97ad6c
Michael-Van-Robbe <mittermayrm180114@sus.htl-grieskirchen.at>
2021-09-26 21:19:23 +0200

    Arraylists with all prime numbers and all even numbers were created

--- src/net/htlgkr/mittermayrm180114/beispiel2/Main.java
+++ src/net/htlgkr/mittermayrm180114/beispiel2/Main.java
@@ -1,6 +1,30 @@
 package net.htlgkr.mittermayrm180114.beispiel2;
 
+import java.util.ArrayList;
+
 public class Main
 {
+    public static void main(String[] args)
+    {
+        int upperLimit = 100;
+        EratosthenesPrimeSieve eps = new EratosthenesPrimeSieve(upperLimit);
+        ArrayList<Integer> primeNumbers = new ArrayList<>();
+        ArrayList<Integer> evenNumbers = new ArrayList<>();
 
+        boolean isPrime = true;
+        for(int i = 0; i < upperLimit; i++)
+        {
+            isPrime = eps.isPrime(i);
+
+            if(isPrime == true)
+            {
+                primeNumbers.add(i);
+            }
+        }
+
+        for(int i = 4; i < upperLimit; i+=2)
+        {
+            evenNumbers.add(i);
+        }
+    }
 }
